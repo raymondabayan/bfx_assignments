6732b331668ed8da00455edd82bec98cc89fb38e
raymond <raymond-john_abayan@bio-rad.com>
2022-07-12 21:23:16 -0700

    initial commit

---
+++ hw01/assignment_01.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import sys
+
+os.listdir()
+names = pd.read_csv("peak_names_out.csv").head()
+counts = pd.read_csv("count_matrix.csv").head()
