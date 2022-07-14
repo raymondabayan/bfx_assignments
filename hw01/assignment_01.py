1fe6a8c67f3b3dd9834d5d19bb4395e911ce92df
raymond <raymond-john_abayan@bio-rad.com>
2022-07-14 09:38:54 -0700

    added separation of human and mouse datasets

--- hw01/assignment_01.py
+++ hw01/assignment_01.py
@@ -12,6 +12,37 @@ in_names = pd.read_csv("peak_names_out.csv", header = None, chunksize = 500)
 in_counts = pd.read_csv("count_matrix.csv", chunksize = 500)
 counts = pd.concat(in_counts, ignore_index=True)
 names = pd.concat(in_names, ignore_index=True)
+data = pd.merge(names, counts, left_index= True, right_index= True)
+
+split_names = []
+
+string = "hg19_chr1:713971-714221"
+human = "hg19"
+mouse = "mm10"
+string.split("_")
+names[0]
+type(string)
+
+if human in string:
+    print("Found")
+else:
+    print("Not found")
+
+human_data = pd.DataFrame()
+mouse_data = pd.DataFrame()
+
+for name in names[0]:
+    split_names.append(name.split("_"))
+
+split_names
+
+for name in names[0]:
+    if human in name:
+        human_data.append(human)
+    elif mouse in name:
+        mouse_data.append(mouse)
+
+.split("_")
 data = pd.merge(names, counts, left_index = True, right_index = True)
 
 has1 = data.iloc[:, 1:-1] == 1
